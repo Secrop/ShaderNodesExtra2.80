b173463344d7bb70f133ea63d47ea747ac10a898
Secrop <porces@gmail.com>
2017-04-15 16:06:50 +0200

    Version 1.5
    ShaderNodeBase is functional.

---
+++ Nodes/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+ignore_list=['__init__.py', 'ShaderNodeBase.py'] 
+def listNodes():
+    nodelist=[]
+    for subdir, dirs, files in os.walk(os.path.dirname(__file__)):
+        current_dir = os.path.basename(subdir)
+        if current_dir == '__pycache__':
+            continue
+        for file in files:
+            if file in ignore_list:
+                continue
+            if not file.endswith('.py'):
+                continue
+            nodelist.append(file[:-3])
+    return nodelist
