7bb5cdc7b0a274cbb7154badbf7278ed93e2027f
Secrop <porces@gmail.com>
2017-04-21 16:20:22 +0200

    Update __init__.py

--- Nodes/__init__.py
+++ Nodes/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 ignore_list=['__init__.py', 'ShaderNodeBase.py'] 
@@ -15,21 +14,3 @@ def listNodes():
                 continue
             nodelist.append(file[:-3])
     return nodelist
-=======
-import os
-
-ignore_list=['__init__.py', 'ShaderNodeBase.py'] 
-def listNodes():
-    nodelist=[]
-    for subdir, dirs, files in os.walk(os.path.dirname(__file__)):
-        current_dir = os.path.basename(subdir)
-        if current_dir == '__pycache__':
-            continue
-        for file in files:
-            if file in ignore_list:
-                continue
-            if not file.endswith('.py'):
-                continue
-            nodelist.append(file[:-3])
-    return nodelist
->>>>>>> master
